481e9409e7edcec68a5eb48eea88523cd95d536a
saicharan2005 <kundurusaicharan@gmail.com>
2025-07-27 15:52:59 +0530

    made serverless

---
+++ backend/api/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("../config/db");
+
+const authRoutes = require("../routes/authRoutes");
+const incomeRoutes = require("../routes/incomeRoutes");
+const expenseRoutes = require("../routes/expenseRoutes");
+const dashboardRoutes = require("../routes/dashboardRoutes");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "https://personal-finance-manager-9ijl.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/income", incomeRoutes);
+app.use("/api/expense", expenseRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+module.exports = app;
+module.exports.handler = serverless(app);
